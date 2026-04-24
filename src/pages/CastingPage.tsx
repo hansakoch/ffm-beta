7e79b332c089e4852751fb8bfba8792809e4c0ef
Hans Al Koch (HAK) <16804056+hansakoch@users.noreply.github.com>
2026-04-25 04:41:57 +0800

    Update Casting Page hero image

--- src/pages/CastingPage.tsx
+++ src/pages/CastingPage.tsx
@@ -48,7 +48,7 @@ const CastingPage = () => {
       <div
         className="relative min-h-[90vh] flex items-center justify-center py-20"
         style={{
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.65), rgba(15, 23, 42, 0.6)), url('/jakob-owens-xKfS7Hll0Ck-unsplash.jpg')`,
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.65), rgba(15, 23, 42, 0.6)), url('/travis-colbert-fz2Am8mQfEw-unsplash.jpg')`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat'
