a12f893a8c9b19864c8e5ceed0b5219fe596375d
Hans Al Koch (HAK) <16804056+hansakoch@users.noreply.github.com>
2026-04-25 07:36:32 +0800

    Update logo size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,7 +38,7 @@ const Header = () => {
             <img
               src="/artboard_1_copy.png"
               alt="FansFollowMe - Global Fitness & Martial Arts Creator Platform Logo"
-              className="h-12 w-auto"
+              className="h-8 w-auto"
               width="180"
               height="48"
             />
