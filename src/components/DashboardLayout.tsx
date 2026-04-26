44ca919a90aecc01ecdaf1c98f4487d33d3a670c
Hans Al Koch (HAK) <16804056+hansakoch@users.noreply.github.com>
2026-04-26 08:49:23 +0800

    Updated DashboardLayout.tsx

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -79,7 +79,7 @@ const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
           </Link>
 
           {/* Center: Search */}
-          <div className="flex-1 max-w-2xl mx-8">
+          <div className="flex-1 max-w-2xl mx-2 sm:mx-8 hidden sm:block">
             <div className="relative">
               <Search className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-500" size={20} />
               <input
